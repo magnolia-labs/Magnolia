7dc23cff350fdffb07a336b40d165b8f612fe650
Alice Wong <alicewong@Alices-MacBook-Pro.local>
2019-05-18 15:52:07 -0400

    Add panel to canvas

--- client/containers/panel.js
+++ client/containers/panel.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Panel = () => {
+const Panel = ({ node_id }) => {
+    console.log('node in panel ', node_id)
     return (
       <RightPanel>
         <form>
-            <label htmlFor="ProjectName">Project Name: </label>
-            <input type="text" defaultValue="Enter project name"></input>
-            <label htmlFor="Stateful">Is the component stateful? </label>
-            <input type="checkbox" name="Stateful" onChange={console.log('changed pref')} defaultChecked></input>
+            <div>
+              <label htmlFor="ProjectName">Project Name: </label>
+              <input type="text" defaultValue="My Project"></input>
+            </div>
+            <div>
+              <label htmlFor="ComponentName">Component Name: </label>
+              <input type="text" defaultValue={node_id}></input>
+            </div>
+            <div>
+              <label htmlFor="Stateful">Is the component stateful? </label>
+              <input type="checkbox" name="Stateful" onChange={console.log('')} defaultChecked></input>
+            </div>
+            <div>
+              <label htmlFor="Count">Count: </label>
+              <input type="text" defaultValue="1"></input>
+            </div>
+            <div>
+              <label htmlFor="Props">Props: </label>
+              <input type="text" defaultValue=""></input>
+            </div>
+            <button>Update Node</button>
         </form>
       </RightPanel>
     )

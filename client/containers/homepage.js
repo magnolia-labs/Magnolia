72cd1674cb967780a7425bb1fc233da01d0e9c79
Alice Wong <alicewong@Alices-MacBook-Pro.local>
2019-05-18 15:53:27 -0400

    Update homepage to include dropdown

--- client/containers/homepage.js
+++ client/containers/homepage.js
@@ -1,21 +1,20 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import styled from 'styled-components';
 import ProjectCanvas from './project.js';
+import ProjectDropdown from './projectdropdown.js';
 
 const Homepage = (props) => {
-
-    const [age, setAge] = useState(0);
 
     //This state tracks the display of a new project
     const [newProject, setNewProject] = useState(false); 
 
-    //
+    //This state tracks what current project the user is viewing
     const [currentProject, updateCurrentProject] = useState({});
 
     const addNewProject = (props) => {
         setNewProject(true);
-
+        console.log(' i am in here')
         //USER IS CURRENTLY HARDCODED. MUST UPDATE TO REFLECT ACTUAL CURRENT USER
         const metaData = {
             'method': 'POST',
@@ -27,29 +26,29 @@ const Homepage = (props) => {
 
         fetch('/newproject', metaData)
             .then(response => response.json())
-            .then(response => console.log(' response ', response))
             .then(response => {
                 updateCurrentProject({
                     project_id: response.project_id, //confirm with backend naming
-                    head_id: response.head_id,
+                    node_id: response.head_id,
                     stateful: response.stateful,
                     state_props: response.state_props,
                     count: response.count,
-                    children: []
+                    children: [{ node_id: 4}, { node_id: 3}, { node_id: 5},]
                 })
             })
-            .catch(err => console.log(err))
+            .catch(err => console.log('err', err))
 
-    }
+    }  
 
     return (
       <React.Fragment>
-        <NewProjectBtn onClick={addNewProject}>New Project</NewProjectBtn>
-        <form>
-            <select>    
+        { !newProject && <NewProjectBtn onClick={addNewProject}>New Project</NewProjectBtn>}
+        { !newProject && <ProjectDropdown />}
+       <form>
+            {/* <select>    
                 <option value="project1">Project One</option>
                 <option value="project2">Project Two</option>
-            </select>
+            </select> */}
         </form>
         { newProject && <ProjectCanvas project={currentProject}/> }
       </React.Fragment>
@@ -59,6 +58,10 @@ const Homepage = (props) => {
 export default Homepage;
 
 const NewProjectBtn = styled.button`
+
+    padding: 10px;
+    border-radius: 3px;
+
     :focus {
         outline: none;
     }

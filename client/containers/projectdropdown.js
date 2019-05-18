160f6a9d6395246f1cbb1dbafc125704fa6ff3de
Alice Wong <alicewong@Alices-MacBook-Pro.local>
2019-05-18 15:52:32 -0400

    Add project dropdown list

---
+++ client/containers/projectdropdown.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ProjectDropdown = () => {
+
+    return (
+        <select>
+            <option>one</option>
+        </select>
+    )
+}
+
+export default ProjectDropdown;
